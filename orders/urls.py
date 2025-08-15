b3e96b3517dd9b985324e836f0051d61e865e6d8
mddarif <144717402+mddarif@users.noreply.github.com>
2025-08-15 14:42:45 +0530

    31193

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import *
 
+def urlpatterns(request):
+
 urlpatterns = [
+    {"name":"Margherita Pizza","price":299},
+    {"name":"Pasta Alfredo","price":349},
+    {"name":"Caesar Salad", "Price":149},
     
 ]
+return render (request,"menu_list.html",{"menu_items":menu_items})
